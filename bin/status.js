9c34936b0a274af1314938fbd985a0680406d150
coderaiser <mnemonic.enemy@gmail.com>
2014-11-14 10:27:31 -0500

    feature(status-io) add lib

---
+++ bin/status.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+
+(function(){
+    'use strict';
+    
+    var http        = require('http'),
+        express     = require('express'),
+        files       = require('files-io'),
+        
+        DIR         = __dirname + '/../',
+        
+        status      = require('../lib/status-img'),
+        
+        PORT        = 4321,
+        app         = express();
+    
+    http.createServer(app).listen(PORT);
+    
+    console.log('server: ' + PORT + '\npid: ' + process.pid);
+    
+    app.get('/', function(req, res) {
+        sendFile(res, DIR + 'HELP.md');
+    });
+    
+    app.get('/host(/*)?', function(request, response) {
+        var addr    = request.params[1],
+            host    = 'http://' + addr;
+        
+        if (!addr)
+            response.send('/host/:address');
+        else
+            status(host, response);
+    });
+    
+    function sendFile(res, name) {
+        files.pipe(name, res, function(error) {
+            if (error)
+                res.send(error.message);
+        });
+    }
+})();
