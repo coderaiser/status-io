eb048696a28ad39d13c6f45a334dfe2b9fd20006
coderaiser <coderaiser@cloudcmd.io>
2016-11-03 13:53:09 +0200

    chore(status) rm iife

--- bin/status.js
+++ bin/status.js
@@ -1,43 +1,41 @@
 #!/usr/bin/env node
 
-(function(){
-    'use strict';
+'use strict';
+
+var http        = require('http'),
+    express     = require('express'),
+    files       = require('files-io'),
     
-    var http        = require('http'),
-        express     = require('express'),
-        files       = require('files-io'),
-        
-        DIR         = __dirname + '/../',
-        
-        status      = require('../lib/status-img'),
-        
-        PORT        = 4321,
-        app         = express();
+    DIR         = __dirname + '/../',
     
-    http.createServer(app).listen(PORT);
+    status      = require('../lib/status-img'),
     
-    console.log('server: ' + PORT + '\npid: ' + process.pid);
+    PORT        = 4321,
+    app         = express();
+
+http.createServer(app).listen(PORT);
+
+console.log('server: ' + PORT + '\npid: ' + process.pid);
+
+app.use('/', express.static(DIR));
+  
+app.get('/', function(req, res) {
+    sendFile(res, DIR + 'HELP.md');
+});
+
+app.get('/host(/*)?', function(request, response) {
+    var addr    = request.params[1],
+        host    = 'http://' + addr;
     
-    app.use('/', express.static(DIR));
-      
-    app.get('/', function(req, res) {
-        sendFile(res, DIR + 'HELP.md');
+    if (!addr)
+        response.send('/host/:address');
+    else
+        status(host, response);
+});
+
+function sendFile(res, name) {
+    files.pipe(name, res, function(error) {
+        if (error)
+            res.send(error.message);
     });
-    
-    app.get('/host(/*)?', function(request, response) {
-        var addr    = request.params[1],
-            host    = 'http://' + addr;
-        
-        if (!addr)
-            response.send('/host/:address');
-        else
-            status(host, response);
-    });
-    
-    function sendFile(res, name) {
-        files.pipe(name, res, function(error) {
-            if (error)
-                res.send(error.message);
-        });
-    }
-})();
+}
